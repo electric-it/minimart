d382b2d837a55bc61c6db5bf75c6ceafaad9f997
Richard Hall <richardardrichard@gmail.com>
2014-11-24 11:03:22 -0500

    Updating source to work with the extracted universe code

--- lib/minimart/mirror/source.rb
+++ lib/minimart/mirror/source.rb
@@ -20,45 +20,16 @@ module Minimart
 
       private
 
-      def universe
-        @universe ||= fetch_universe
-      end
-
-      def fetch_universe
-        Minimart::Configuration.output.puts "Fetching the universe for #{url} ..."
-        universe = JSON.parse(RestClient.get(URI.join(url, '/universe').to_s))
-
-        result = []
-        universe.each do |cookbook_name, versions|
-          versions.each do |version, attrs|
-            result << Hashie::Mash.new(
-              name:          cookbook_name,
-              version:       version,
-              dependencies:  attrs['dependencies'],
-              location_path: attrs['location_path'],
-              download_url:  attrs['download_url'])
-          end
-        end
-
-        result
-      end
-
       def fetch_cookbook(cookbook_name, cookbook_version, &block)
         Minimart::Configuration.output.puts "Downloading #{cookbook_name} #{cookbook_version}"
-        cookbook = find_cookbook(cookbook_name, cookbook_version)
+        cookbook = universe.find_cookbook(cookbook_name, cookbook_version)
 
         block.call(cookbook, download_cookbook_archive_file(cookbook))
 
         cookbook.dependencies.each do |dependency_name, dependency_requirements|
-          version = resolve_dependency(dependency_name, dependency_requirements)
+          version = universe.resolve_dependency(dependency_name, dependency_requirements)
           fetch_cookbook(dependency_name, version, &block)
         end
-      end
-
-      def find_cookbook(cookbook_name, version)
-        universe.select do |cookbook|
-          cookbook.name == cookbook_name && cookbook.version == version
-        end.first
       end
 
       def download_cookbook_archive_file(cookbook)
@@ -69,16 +40,8 @@ module Minimart
         result
       end
 
-      def resolve_dependency(cookbook_name, requirements)
-        Solve.it!(dependency_graph, [[cookbook_name, requirements]])[cookbook_name]
-      end
-
-      def dependency_graph
-        @dependency_graph ||= Solve::Graph.new.tap do |graph|
-          universe.each do |remote_cookbook|
-            graph.artifact(remote_cookbook.name, remote_cookbook.version)
-          end
-        end
+      def universe
+        @universe ||= Universe.new(url)
       end
 
     end
