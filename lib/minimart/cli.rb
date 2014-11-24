3a960c86a37f040e3ece2ce6c37a80f0a59b0ef3
Richard Hall <richardardrichard@gmail.com>
2014-11-24 16:35:01 -0500

    Removing misleading mirror CLI description

--- lib/minimart/cli.rb
+++ lib/minimart/cli.rb
@@ -3,7 +3,7 @@ require 'minimart'
 module Minimart
   class Cli < Thor
 
-    desc 'mirror [CONFIG_FILE]', 'Mirror a listing of cookbooks.'
+    desc 'mirror', 'Mirror a listing of cookbooks.'
     option :inventory_config, default: Minimart::Configuration::DEFAULT_INVENTORY_CONFIG
     option :inventory_directory, default: Minimart::Configuration::DEFAULT_INVENTORY_DIRECTORY
     def mirror
