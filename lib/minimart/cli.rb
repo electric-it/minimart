eecda4774301456ae98bc1f5ee6279873e85510f
Richard Hall <richardardrichard@gmail.com>
2014-11-26 15:07:46 -0500

    Adding a very basic init command

--- lib/minimart/cli.rb
+++ lib/minimart/cli.rb
@@ -2,6 +2,28 @@ require 'minimart'
 
 module Minimart
   class Cli < Thor
+    include Thor::Actions
+
+    desc 'init', 'Begin a new Minimart.'
+    def init
+      create_file './inventory.yml' do
+<<-YML
+# sources:
+#   - "https://supermarket.getchef.com"
+# cookbooks:
+#   mysql:
+#     versions:
+#       - "~> 4.0.2"
+#       - "> 5.0.0"
+#   yum:
+#     type: git
+#     branches:
+#       - a_branch_name
+#     refs:
+#       - SHA
+YML
+      end
+    end
 
     desc 'mirror', 'Mirror a listing of cookbooks.'
     option :inventory_config, default: Minimart::Configuration::DEFAULT_INVENTORY_CONFIG
