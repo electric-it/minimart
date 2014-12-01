7f5c80886352c2ebf74f394c9552d4333ac264a7
Richard Hall <richardardrichard@gmail.com>
2014-12-01 11:04:12 -0500

    Reworking Source to be similar to the Berkshelf definition of a "Source". A "Source" manages cookbooks, behind the Berkshelf API.

---
+++ lib/minimart/mirror/source.rb
@@ -0,0 +1,50 @@
+module Minimart
+  class Mirror
+    class Source
+
+      attr_accessor :base_url
+
+      def initialize(base_url)
+        @base_url = base_url
+      end
+
+      def find_cookbook(cookbook_name, version)
+        index = cookbooks.find_index do |cookbook|
+          cookbook.name == cookbook_name && cookbook.version == version
+        end
+
+        cookbooks[index] unless index.nil?
+      end
+
+      def cookbooks
+        @cookbooks ||= fetch_universe_data.each_with_object([]) do |cookbook_info, memo|
+          name, versions = cookbook_info
+          memo.concat versions.map { |version, attrs| build_cookbook(name, version, attrs) }
+        end
+      end
+
+      private
+
+      def build_cookbook(name, version, attrs)
+        RemoteCookbook.new(attrs.merge(name: name, version: version))
+      end
+
+      def fetch_universe_data
+        Configuration.output.puts "Fetching the universe for #{universe_url} ..."
+
+        Utils::Http.get_json(universe_url)
+
+      rescue RestClient::ResourceNotFound
+        raise UniverseNotFoundError.new "no universe found for #{base_url}"
+      end
+
+      def universe_url
+        src_url = base_url[-1, 1] == '/' ? base_url : "#{base_url}/"
+        URI.join("#{base_url}/", 'universe')
+      end
+    end
+
+    class UniverseNotFoundError < Exception; end
+    class DependencyNotMet < Exception; end
+  end
+end
