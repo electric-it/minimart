87b43b08df040a978ded0ce37fa04c413ce691c6
Richard Hall <richardardrichard@gmail.com>
2014-12-01 14:33:21 -0500

    Initializing specs for utility classes

---
+++ spec/lib/minimart/utils/archive_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Minimart::Utils::Archive do
+  describe '::extract_archive' do
+    pending
+  end
+
+  describe '::extract_cookbook' do
+    pending
+  end
+end
---
+++ spec/lib/minimart/utils/file_helper_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Minimart::Utils::FileHelper do
+  describe '::cookbook_path_in_directory' do
+    pending
+  end
+
+  describe '::find_cookbook_directory' do
+    pending
+  end
+
+  describe '::cookbook_file_in_path' do
+    pending
+  end
+
+  describe '::file_exists' do
+    pending
+  end
+
+  describe '::make_temporary_directory' do
+    pending
+  end
+
+  describe '::make_directory' do
+    pending
+  end
+
+  describe '::remove_directory' do
+    pending
+  end
+
+  describe '::move_directory' do
+    pending
+  end
+
+  describe '::copy_directory' do
+    pending
+  end
+  
+end
