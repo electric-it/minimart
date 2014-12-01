2d3cbcb346fc9fdc2e603cc182982ea8267cd728
Richard Hall <richardardrichard@gmail.com>
2014-12-01 10:57:06 -0500

    Adding a dependency graph class used for managing/resolving requirements

--- lib/minimart/mirror/dependency_graph.rb
+++ lib/minimart/mirror/dependency_graph.rb
@@ -1,26 +1,60 @@
+##
+# A class for managing a listing of remote cookbooks and their dependencies.
+# This can be used to resolve any requirements found in the inventory file.
+##
 module Minimart
-  module Mirror
+  class Mirror
     class DependencyGraph
 
-      attr_reader :graph
+      attr_reader :graph,
+                  :inventory_requirements
 
       def initialize
         @graph = Solve::Graph.new
+        @inventory_requirements = []
       end
 
-      def add_cookbooks(cookbooks)
-        cookbooks.each do |cookbook|
-          next if graph.artifact?(cookbook.name, cookbook.version)
 
-          graph.artifact(cookbook.name, cookbook.version)
+      def add_remote_cookbook(cookbook)
+        return if remote_cookbook_added?(cookbook)
 
-          cookbook.dependencies.each do |dependency|
-            name, requirements = dependency
-            graph.artifact(cookbook.name, cookbook.version).depends(name, requirements)
-          end
+        graph.artifact(cookbook.name, cookbook.version)
+
+        cookbook.dependencies.each do |dependency|
+          name, requirements = dependency
+          graph.artifact(cookbook.name, cookbook.version).depends(name, requirements)
         end
       end
 
+      def remote_cookbook_added?(cookbook)
+        graph.artifact?(cookbook.name,  cookbook.version)
+      end
+
+      def find_graph_artifact(cookbook)
+        graph.find(cookbook.name, cookbook.version)
+      end
+
+      def add_inventory_requirement(requirement)
+        inventory_requirements << requirement
+      end
+
+      def resolved_requirements
+        inventory_requirements.inject([]) do |result, requirement|
+          result.concat(resolve_requirement(requirement).to_a)
+        end
+      end
+
+      private
+
+      def resolve_requirement(requirement)
+        Solve.it!(graph, [[requirement.name, requirement.version_requirement]])
+
+      rescue Solve::Errors::NoSolutionError => e
+        raise UnresolvedDependency, e.message
+      end
+
     end
+
+    class UnresolvedDependency < Exception; end
   end
 end
