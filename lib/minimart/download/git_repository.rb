0caded24beb6556fcc599c3d456b5af52ea132b6
Richard Hall <richardardrichard@gmail.com>
2014-12-02 14:54:28 -0500

    Adding a class specializing in downloading git repos

---
+++ lib/minimart/download/git_repository.rb
@@ -0,0 +1,25 @@
+module Minimart
+  module Download
+    class GitRepository
+
+      attr_reader :url
+
+      def initialize(url)
+        @url = url
+      end
+
+      def download(commitish)
+        bare_repo_path = Dir.mktmpdir
+        bare_repo      = Git.clone(url, bare_repo_path, bare: true)
+        revision       = bare_repo.revparse(commitish)
+
+        result = Dir.mktmpdir
+        result_repo = Git.clone(bare_repo_path, result)
+        result_repo.fetch(bare_repo_path, tags: true)
+        result_repo.reset_hard(revision)
+        result
+      end
+
+    end
+  end
+end
