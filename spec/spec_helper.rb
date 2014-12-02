66803290a7d67ca1686dbabf70cfa5b137f448df
Richard Hall <richardardrichard@gmail.com>
2014-12-02 10:01:00 -0500

    Adding tmp directory for testing file system manipulations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,14 @@ require 'pry'
 require 'webmock/rspec'
 
 require 'minimart'
-require 'fakefs/safe'
+
+Dir['spec/support/*.rb'].each { |f| require File.expand_path(f) }
 
 RSpec.configure do |config|
+  config.include Minimart::RSpecSupport::FileSystem
+
+  config.before(:each) { make_test_directory }
+  config.after(:each) { remove_test_directory }
+
   config.mock_with :rspec
 end
